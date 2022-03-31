7fba23d11066ad86c52d7a87bc8fa9e19cf1fd35
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-30 23:42:23 -0600

    Agregar archivos de proyecto.

---
+++ Partiendo_Cadenas_Strings/Program.cs
@@ -0,0 +1,7 @@
+﻿string str = "Bienvenidos a el mundo de la programacion";
+string[] substr = str.Split(' ');
+Console.WriteLine($"{substr[0]} {substr[1]} {substr[2]} {substr[3]} {substr[4]} {substr[5]} {substr[6]}");
+Console.WriteLine($"{substr[0]} {substr[1]}");
+Console.WriteLine($"{substr[2]} {substr[3]} {substr[4]}");
+Console.WriteLine($"{substr[5]} {substr[6]}");
+Console.ReadKey(true);
